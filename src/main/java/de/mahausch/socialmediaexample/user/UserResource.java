bd4bdbe2ad44901a99b257e95cbddc50813711f9
Markus Hauschild <mhauschild@united-internet.de>
2018-09-03 16:18:57 +0200

    Add createUser method

--- src/main/java/de/mahausch/socialmediaexample/user/UserResource.java
+++ src/main/java/de/mahausch/socialmediaexample/user/UserResource.java
@@ -4,6 +4,8 @@ package de.mahausch.socialmediaexample.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,10 @@ public class UserResource {
     @GetMapping(path="/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
+    }
+
+    @PostMapping(path="/users")
+    public void createUser(@RequestBody User user){
+        service.save(user);
     }
 }
