dafd543906925972be628ae9db007293d3fccc16
Markus Hauschild <mhauschild@united-internet.de>
2018-09-03 16:38:32 +0200

    Create response for createUser method

--- src/main/java/de/mahausch/socialmediaexample/user/UserResource.java
+++ src/main/java/de/mahausch/socialmediaexample/user/UserResource.java
@@ -2,12 +2,15 @@ package de.mahausch.socialmediaexample.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,7 +30,12 @@ public class UserResource {
     }
 
     @PostMapping(path="/users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
