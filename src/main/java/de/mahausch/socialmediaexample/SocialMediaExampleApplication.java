ed6114c5450f3f6ea2e6f97300ec641479b13b9a
Markus Hauschild <mhauschild@united-internet.de>
2018-09-03 14:34:18 +0200

    init

---
+++ src/main/java/de/mahausch/socialmediaexample/SocialMediaExampleApplication.java
@@ -0,0 +1,12 @@
+package de.mahausch.socialmediaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaExampleApplication.class, args);
+	}
+}
