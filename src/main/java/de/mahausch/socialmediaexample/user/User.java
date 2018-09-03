8cfb5b1e11327686f6c948e2e66480305575693a
Markus Hauschild <mhauschild@united-internet.de>
2018-09-03 15:42:13 +0200

    Create User entity and UserDaoService to simulate a database

---
+++ src/main/java/de/mahausch/socialmediaexample/user/User.java
@@ -0,0 +1,45 @@
+package de.mahausch.socialmediaexample.user;
+
+import java.util.Date;
+
+public class User {
+
+    private Integer id;
+    private String user;
+    private Date birthDate;
+
+
+    public User(Integer id, String user, Date birthDate) {
+        this.id = id;
+        this.user = user;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override public String toString() {
+        return "User{" + "id=" + id + ", user='" + user + '\'' + ", birthDate=" + birthDate + '}';
+    }
+}
