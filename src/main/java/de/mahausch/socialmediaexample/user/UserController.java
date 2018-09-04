cf1bb1fe138d103a207ce9e74fb77acc934d5df3
Markus Hauschild <mhauschild@united-internet.de>
2018-09-04 11:21:26 +0200

    Add delete methods

--- src/main/java/de/mahausch/socialmediaexample/user/UserController.java
+++ src/main/java/de/mahausch/socialmediaexample/user/UserController.java
@@ -4,6 +4,7 @@ package de.mahausch.socialmediaexample.user;
 import de.mahausch.socialmediaexample.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -34,13 +36,20 @@ public class UserController {
         return user;
     }
 
-    @PostMapping(path="/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user){
+    @PostMapping(path="/users") public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}").buildAndExpand(savedUser.getId())
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(path = "/users/{id}") public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
     }
 }
